d1d83849554c38fba3ae0b6cfd7f1645ab86eacf
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-06-21 22:14:45 +0000

    Creating function to calculate at every comma

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,19 @@
-const stringCalculator = (stringInput) => {
+const stringCalculator = (stringInput = 0) => {
+  if (stringInput === 0) {
+    return 0;
+  }
 
+  const splitStringInput = stringInput.split(",")
+  // console.log(splitStringInput)
+  let calculator = 0;
+  for(let i = 0; i < splitStringInput.length; i++) {
+    // console.log(typeof Number(splitStringInput[i]) === "number")
+    if (typeof Number(splitStringInput[i]) === "number") {
+      calculator += Number(splitStringInput[i])
+    }
+  }
+  // console.log(calculator)
+  return calculator;
 }
 
 module.exports = {stringCalculator};
--- test/test_stringCalculator.js
+++ test/test_stringCalculator.js
@@ -1,4 +1,16 @@
-const chai = require('chai');
-const assert = chai.assert;
+const chai = require("chai");
+const expect = chai.expect;
 
-const {stringCalculator} = require('../stringCalculator.js')
+const { stringCalculator } = require("../stringCalculator.js");
+
+describe("Step 1", function () {
+  it("should return 0 when the input is empty", () => {
+    expect(stringCalculator()).to.equal(0);
+  });
+  it("return type should be an integer", () => {
+    expect(typeof stringCalculator()).to.equal("number");
+  });
+  it("input of '1,2,5' should result in 8", () => {
+    expect(stringCalculator("1,2,5")).to.equal(8);
+  });
+});
