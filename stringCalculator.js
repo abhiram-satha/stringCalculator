9d6a13683d17dcb4068716f9c59bc996b926a2c5
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-06-22 00:48:27 +0000

    Removing console.log

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,17 +1,16 @@
 const stringCalculator = (stringInput = 0) => {
-  // stringInput = stringInput.toLowerCase().replace(/[ *_#]/g, '');
   if (stringInput === 0) {
     return 0;
   }
 
   const splitStringInput = stringInput.split(",");
-  console.log(splitStringInput);
+
   let calculator = 0;
   let negativeStatement = "Negatives not allowed, ";
   const integers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
   for (let i = 0; i < splitStringInput.length; i++) {
-    // console.log(typeof Number(splitStringInput[i]) === "number")
+
     if (Number(splitStringInput[i]) < 0) {
       negativeStatement += `${Number(splitStringInput[i])}, `;
     }
@@ -26,14 +25,14 @@ const stringCalculator = (stringInput = 0) => {
         if (integers.includes(splitStringInput[i][j])) {
           console.log("num", Number(splitStringInput[i][j]));
           calculator += Number(splitStringInput[i][j]);
-          // console.log(calculator);
+
         }
       }
-      // console.log(('number',splitStringInput[i]).length)
+
     }
-    // console.log("length",splitStringInput[i].length)
+
   }
-  // console.log(calculator)
+
   if (negativeStatement.length > 24) {
     return negativeStatement;
   }
