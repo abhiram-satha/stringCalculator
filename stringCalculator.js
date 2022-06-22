bbd375cd30e40128ee0c9026b3130ee6dd51a266
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-06-22 00:16:04 +0000

    Uncommenting tests

--- stringCalculator.js
+++ stringCalculator.js
@@ -7,18 +7,20 @@ const stringCalculator = (stringInput = 0) => {
   const splitStringInput = stringInput.split(",");
   console.log(splitStringInput);
   let calculator = 0;
-  let negativeStatement = "Negatives not allowed, "
+  let negativeStatement = "Negatives not allowed, ";
+  const integers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
   for (let i = 0; i < splitStringInput.length; i++) {
     // console.log(typeof Number(splitStringInput[i]) === "number")
     if (Number(splitStringInput[i]) < 0) {
-      negativeStatement += `${Number(splitStringInput[i])}, `
+      negativeStatement += `${Number(splitStringInput[i])}, `;
     }
 
     if (typeof Number(splitStringInput[i]) === "number") {
       calculator += Number(splitStringInput[i]);
     }
-    const integers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    if (splitStringInput[i].length > 1) {
+    
+    else if (splitStringInput[i].length > 1) {
       for (let j = 0; j < splitStringInput[i].length; j++) {
         if (integers.includes(splitStringInput[i][j])) {
           console.log("num", Number(splitStringInput[i][j]));
@@ -31,8 +33,8 @@ const stringCalculator = (stringInput = 0) => {
     // console.log("length",splitStringInput[i].length)
   }
   // console.log(calculator)
-  if (negativeStatement.length > 22) {
-    return negativeStatement
+  if (negativeStatement.length > 24) {
+    return negativeStatement;
   }
   return calculator.toString();
 };
--- test/test_stringCalculator.js
+++ test/test_stringCalculator.js
@@ -3,31 +3,31 @@ const expect = chai.expect;
 
 const { stringCalculator } = require("../stringCalculator.js");
 
-// describe("Step 1", function () {
-//   it("should return 0 when the input is empty", () => {
-//     expect(stringCalculator()).to.equal(0);
-//   });
-//   it("return type should be an integer", () => {
-//     expect(typeof stringCalculator()).to.equal("number");
-//   });
-//   it("input of '1,2,5' should result in 8", () => {
-//     expect(stringCalculator("1,2,5")).to.equal("8");
-//   });
-// });
-// describe("Step 2", function () {
-//   it("should return 6 when the input is '1\n,2,3'", () => {
-//     expect(stringCalculator("1\n,2,3")).to.equal("6");
-//   });
-//   it("should return 7 when the input is '1,\n2,4'", () => {
-//     expect(stringCalculator("1,\n2,4")).to.equal("7");
-//   });
-// });
-// describe("Step 3", function () {
-//     it("should return 8 when the input is '“//;\n1;3;4”'", () => {
-//       expect(stringCalculator("//;\n1;3;4")).to.equal("8");
-//     });
+describe("Step 1", function () {
+  it("should return 0 when the input is empty", () => {
+    expect(stringCalculator()).to.equal(0);
+  });
+  it("return type should be an integer", () => {
+    expect(typeof stringCalculator()).to.equal("number");
+  });
+  it("input of '1,2,5' should result in 8", () => {
+    expect(stringCalculator("1,2,5")).to.equal("8");
+  });
+});
+describe("Step 2", function () {
+  it("should return 6 when the input is '1\n,2,3'", () => {
+    expect(stringCalculator("1\n,2,3")).to.equal("6");
+  });
+  it("should return 7 when the input is '1,\n2,4'", () => {
+    expect(stringCalculator("1,\n2,4")).to.equal("7");
+  });
+});
+describe("Step 3", function () {
+    it("should return 8 when the input is '“//;\n1;3;4”'", () => {
+      expect(stringCalculator("//;\n1;3;4")).to.equal("8");
+    });
 
-// });
+});
 describe("Step 4", function () {
   it("input of '-1,2,5' should result in Negatives not allowed", () => {
     expect(stringCalculator("-1,2,5")).to.equal("Negatives not allowed, -1, ");
